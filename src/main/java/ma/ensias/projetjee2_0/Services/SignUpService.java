274d1f8006ae3233a06490b68175b19f356a7f47
AJOthmane <aja.oth@gmail.com>
2021-06-04 18:16:42 +0100

    adding signup route

--- src/main/java/ma/ensias/projetjee2_0/Services/SignUpService.java
+++ src/main/java/ma/ensias/projetjee2_0/Services/SignUpService.java
@@ -13,21 +13,17 @@ import java.util.HashMap;
 
 public class SignUpService {
 
-    public String getMd5(String input)
+    public static String getMd5(String input)
     {
         try {
 
-            // Static getInstance method is called with hashing MD5
             MessageDigest md = MessageDigest.getInstance("MD5");
 
-            // digest() method is called to calculate message digest
-            //  of an input digest() return array of byte
             byte[] messageDigest = md.digest(input.getBytes());
 
-            // Convert byte array into signum representation
+
             BigInteger no = new BigInteger(1, messageDigest);
 
-            // Convert message digest into hex value
             String hashtext = no.toString(16);
             while (hashtext.length() < 32) {
                 hashtext = "0" + hashtext;
@@ -35,13 +31,10 @@ public class SignUpService {
             return hashtext;
         }
 
-        // For specifying wrong message digest algorithms
         catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
     }
-
-    // todo : check email form , insert into database , id_session
     public SignUpResponse SignUp(HashMap<String,String> user,UserRepository userRepositoryArg)
     {
         UserRepository userRepository = userRepositoryArg;
@@ -79,8 +72,4 @@ public class SignUpService {
         }
         return  new SignUpResponse(success,errors);
     }
-
-
-
-
 }
--- src/main/java/ma/ensias/projetjee2_0/controllers/SignupController.java
+++ src/main/java/ma/ensias/projetjee2_0/controllers/SignupController.java
@@ -2,10 +2,8 @@ package ma.ensias.projetjee2_0.controllers;
 
 import ma.ensias.projetjee2_0.Responses.SignUpResponse;
 import ma.ensias.projetjee2_0.Services.SignUpService;
-import ma.ensias.projetjee2_0.entites.User;
 import ma.ensias.projetjee2_0.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +21,5 @@ public class SignupController {
     public SignUpResponse SignUp(@RequestBody HashMap<String,String> user )
     {
         return signUpService.SignUp(user,userRepository);
-    }
-    @GetMapping(value="/signup")
-    public String SignUp()
-    {
-        return "GET working well";
     }
 }
