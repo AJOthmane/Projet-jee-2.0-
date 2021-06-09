52897f9f3e5d13ae0f60b389fb383fadd2bad9dd
omarirro <omarchbab@gmail.com>
2021-06-09 01:55:10 +0100

    Fix dates

--- src/main/java/ma/ensias/projetjee2_0/WebConfig.java
+++ src/main/java/ma/ensias/projetjee2_0/WebConfig.java
@@ -1,13 +1,11 @@
 package ma.ensias.projetjee2_0;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
